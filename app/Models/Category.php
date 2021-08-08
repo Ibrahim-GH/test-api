de9179779c6f679e21acc0aeed8ed362fd5c6b40
Ibrahiem <ibrahimalgharraa@gmail.com>
2021-08-08 16:18:21 +0300

    migration  DataBase

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,5 +7,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
-}
+   
+    protected $table = 'categories';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+
+    ########################### The Relation for Category ###############################
+
+//     //the user has many posts and post belong to one user
+//     public function user()
+//     {
+//         return $this->belongsTo(User::class, 'user_id', 'id');
+//     }
+
+//     //the post has many comment and comment belong to one post
+//     public function comments()
+//     {
+//         return $this->hasMany(Comment::class, 'post_id', 'id');
+//     }
+
+//     //the post has many like and like belong to one post
+//     public function likes()
+//     {
+//         return $this->hasMany(Like::class, 'post_id', 'id');
+//     }
+// }
